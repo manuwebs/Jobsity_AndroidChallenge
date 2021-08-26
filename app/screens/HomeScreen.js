0ac2da5e08579e5be2ff1ace1eeddda5d840bcf5
Emmanuel Brito <Emmanuel.Brito@ab-inbev.com>
2021-08-26 14:13:26 -0400

    implement search bar and show list

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,10 +1,49 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+import ShowsAPI from '../api/ShowsAPI';
+import SearchBar from '../components/SearchBar';
+import ShowsList from '../components/ShowsList';
+import useAPI from '../hooks/useAPI';
 
 export default function HomeScreen() {
-  return (
-    <View>
-      <Text>Home screen Works!</Text>
-    </View>
-  );
+  const { data, error, loading, request: getShows } = useAPI(ShowsAPI.get);
+  const [page, setPage] = useState(0);
+  const [shows, setShows] = useState([]);
+  const [searchResults, setSearchResults] = useState(null);
+  const [searchLoading, setSearchLoading] = useState(false);
+
+  // fetch shows everytime page number changes
+  useEffect(() => {
+    getShows(page);
+  }, [page]);
+
+  // updating shows variable everytime data is fetched
+  useEffect(() => {
+    if (data) setShows([...shows, ...data]);
+  }, [data]);
+
+  if (!data && error) {
+    return (
+      <SafeAreaView style={{ flex: 1 }}>
+        <Text>Error fetching data</Text>
+      </SafeAreaView>
+    );
+  } else {
+    return (
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={{ height: 50 }}>
+          <SearchBar
+            searchResults={setSearchResults}
+            onLoading={setSearchLoading}
+            onError={error => console.log(error)}
+          />
+        </View>
+        <ShowsList
+          shows={searchResults ?? shows}
+          onEndReached={() => (!searchResults ? setPage(page + 1) : null)}
+          loading={loading || searchLoading}
+        />
+      </SafeAreaView>
+    );
+  }
 }
