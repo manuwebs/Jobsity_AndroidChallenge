7e311b562e74fcaf94757f1ac5d8677b240abdd0
Emmanuel Brito <Emmanuel.Brito@ab-inbev.com>
2021-08-27 02:13:36 -0400

    add common styles

--- app/utils/CommonStyles.js
+++ app/utils/CommonStyles.js
@@ -1,9 +1,62 @@
+import { StyleSheet } from 'react-native';
+
 export const AppColors = {
   white: '#FFFFFF',
-  black: '#000',
+  black: '#322d2b',
   gray: '#f0efef',
   noFill: 'transparent',
-  primary: '#304999',
-  seconday: '#0c1c3b',
-  accent: '#ffc709',
+  primary: '#f3c6bf',
+  secondary: '#3f8a89',
+  accent: '#a56f67',
 };
+
+const margin = 10;
+
+export const AppStyles = StyleSheet.create({
+  borderRadius: {
+    borderRadius: 15,
+  },
+  heading1: {
+    fontSize: 30,
+  },
+  alignCenter: {
+    alignSelf: 'center',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: AppColors.secondary,
+  },
+  textCenter: {
+    textAlign: 'center',
+  },
+  textJustify: {
+    textAlign: 'justify',
+  },
+  textRight: {
+    textAlign: 'right',
+  },
+  bold: {
+    fontWeight: 'bold',
+  },
+  marginHorizontal: {
+    marginHorizontal: margin,
+  },
+  marginVertical: {
+    marginVertical: margin,
+  },
+  marginBottom: {
+    marginBottom: margin,
+  },
+  marginTop: {
+    marginTop: margin,
+  },
+  marginLeft: {
+    marginLeft: margin,
+  },
+  marginRight: {
+    marginRight: margin,
+  },
+  margin: {
+    margin: margin,
+  },
+});
