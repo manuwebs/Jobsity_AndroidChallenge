af653cab1981ca8d8b2aaac37d5ed5cda0ec9937
Emmanuel Brito <Emmanuel.Brito@ab-inbev.com>
2021-08-27 04:13:20 -0400

    add materialCommunityIcons

---
+++ app/components/AppIcon.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { AppColors } from '../utils/CommonStyles';
+
+export default function AppIcon({ name, size = 20, color = AppColors.white }) {
+  return <Icon name={name} size={size} color={color} />;
+}
