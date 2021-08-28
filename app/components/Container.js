90b58b182a49823bc50292a5f7ab93e7de703f9e
Emmanuel Brito <Emmanuel.Brito@ab-inbev.com>
2021-08-28 03:10:57 -0400

    add refreshing to container on scrollview

--- app/components/Container.js
+++ app/components/Container.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+  RefreshControl,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -8,7 +9,13 @@ import {
 } from 'react-native';
 import { AppColors } from '../utils/CommonStyles';
 
-export default function Container({ children, style, scrollable = false }) {
+export default function Container({
+  children,
+  style,
+  scrollable = false,
+  onRefresh,
+  refreshing,
+}) {
   return (
     <SafeAreaView style={styles.outerContainer}>
       <StatusBar
@@ -17,6 +24,12 @@ export default function Container({ children, style, scrollable = false }) {
       />
       {scrollable ? (
         <ScrollView
+          refreshControl={
+            refreshing !== null && (
+              <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+            )
+          }
+          keyboardShouldPersistTaps={'handled'}
           persistentScrollbar
           style={[styles.innerContainer]}
           contentContainerStyle={style}>
